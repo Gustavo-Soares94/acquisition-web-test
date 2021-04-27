8585ba9361e7a48d5d27f7952112271859704e38
Gustavo Soares <gustavosoares.adm@gmail.com>
2021-04-27 09:06:19 -0300

    Atualizacao

--- src/test/java/test/acquisition.java
+++ src/test/java/test/acquisition.java
@@ -22,5 +22,9 @@ public class acquisition {
         driver.findElement(By.xpath("//input[@name='password']")).sendKeys("teste123");
         driver.findElement(By.xpath("//button[@class='button -block -primary sc-sicredi-button sc-sicredi-button-s']")).click();
 
+        driver.get("https://sicredi--tst.lightning.force.com/lightning/n/Novo_Atendimento");
+
+        driver.findElement(By.xpath("//div[@id='cpf']/lightning-input[@class='slds-form-element']//input[@pattern='^[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}$']")).sendKeys("91856063089");
+
     }
 }
